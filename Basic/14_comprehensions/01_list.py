1af1cf25a96ed845085ed57d38368c1623d41f53
Siegel <2285448337@qq.com>
2024-05-24 18:36:18 +0800

    python comprehensions learning

--- Basic/14_comprehensions/01_list.py
+++ Basic/14_comprehensions/01_list.py
@@ -26,3 +26,16 @@ print(new_names)
 # 计算30以内可以被3整除的整数:
 multiples = [i for i in range(30) if i % 3 == 0]
 print(multiples)
+
+# 每往后写一级,相当于嵌套一级
+list1 = [(x, y) for x in range(10) if x % 2 == 0 for y in range(10) if y % 2 != 0]
+print(list1)
+# 上面的写法等同于下面的写法
+
+list2 = []
+for x in range(10):
+    if x % 2 == 0:
+        for y in range(10):
+            if y % 2 != 0:
+                list2.append((x, y))
+print(list2)
